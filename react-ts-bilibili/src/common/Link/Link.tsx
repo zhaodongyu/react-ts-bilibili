dfdaa0f36395eddc508de1a290b4b96cd60413e7
luckyxts <330995827@qq.com>
2020-12-07 21:46:06 +0800

    上方导航栏

--- react-ts-bilibili/src/common/Link/Link.tsx
+++ react-ts-bilibili/src/common/Link/Link.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 
 
+const ImgDefaultWidth: string = `32px`;
+const ImgDefaultHeight: string = `32px`;
+const defaultFontColor: string = `#fff`;
+
 interface LinkProps{
     img?: any;
     title: string;
+    imgWidth?: string;
+    imgHeight?: string;
+    fontColor?: string;
 }
 
 const Link: React.FC<LinkProps> = (props) => {
-    const { img, title } = props;
+    const { img, title, imgWidth, imgHeight, fontColor } = props;
 
     return (
-        <span className='link' >
-            {img && <img src={img} alt={title} />}
+        <span className='link' style={
+            {color: fontColor ? fontColor: defaultFontColor}
+        }>
+            {img && <img src={img}
+                         style={
+                             {
+                                 width: imgWidth ? imgWidth:ImgDefaultWidth,
+                                 height: imgHeight ? imgHeight:ImgDefaultHeight,
+                             }
+                         }
+                         alt={title} />}
             {title}
         </span>
     )
--- react-ts-bilibili/src/pages/Home/components/Menu/Menu.tsx
+++ react-ts-bilibili/src/pages/Home/components/Menu/Menu.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import IconButton from '../../../../common/IconButton/IconButton';
+import Link from '../../../../common/Link/Link';
 
-
-const primaryData = [
+const menuData = [
     {
         title: `首页`,
         img: require(`./../../../../static/package-secured.png`).default,
@@ -21,20 +21,138 @@ const primaryData = [
     },
 ];
 
+const channelData = [
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+    {
+        title: `动画`,
+        number: 400,
+    },
+];
+
+const friendLinkData = [
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+    {
+        title: `专栏`,
+        img: require(`./../../../../static/akari.jpg`).default,
+    },
+];
 
 interface primaryDataInterface{
     title: string,
     img: any,
 }
 
+interface channelDataInterface{
+    title: string,
+    number: number,
+}
+
+interface friendLinkInterface{
+    title: string,
+    img: any,
+}
 function Menu() {
 
     // 渲染上方导航
-    const renderPrimary = (children: Array<primaryDataInterface>) => {
+    const renderPrimary = (menuData: Array<primaryDataInterface>) => {
         return (
             <ul className='primary-container'>
                 {
-                    children.map( (child, index) => {
+                    menuData.map( (child, index) => {
                         const { title, img } = child;
                         return (
                             <li key={index}>
@@ -45,10 +163,49 @@ function Menu() {
                 }
             </ul>
         )
-    }
+    };
+
+    // 渲染中间分区
+    const renderChannel = (channelData: Array<channelDataInterface>) => {
+        return <div className="channel-container">{channelData.map( (child) => {
+            const { title, number } = child;
+
+            return (<span className="channel-tips">
+                {title}
+                <em>{number}</em>
+            </span>)
+        })}</div>;
+    };
+
+    // 渲染右侧友情链家
+    const renderFriendLink = (friendLinkData: Array<friendLinkInterface>) => {
+        return <div className="friend-link-container">
+            {friendLinkData.map( (child) => {
+                const {title, img} = child;
+                const imgWidth = '25px';
+                const imgHeight = '25px';
+                const fontColor = '#000';
+                return (
+                    <span className="friend-link-tips">
+                        <Link title={title}
+                              img={img}
+                              imgWidth={imgWidth}
+                              imgHeight={imgHeight}
+                              fontColor={fontColor}
+                        />
+                    </span>
+                )
+            })}
+        </div>
+    };
+
     return (
-        <div className='wrap-container'>
-            {renderPrimary(primaryData)}
+        <div className="wrap-container">
+            {renderPrimary(menuData)}
+            <span className="home-line" />
+            {renderChannel(channelData)}
+            <span className="home-line" />
+            {renderFriendLink(friendLinkData)}
         </div>
     );
 }
